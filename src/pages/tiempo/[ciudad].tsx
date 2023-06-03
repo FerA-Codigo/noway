e6a226af49eb58506b3e2594c9758d6437c36109
Fernando Amor <soporte@payfun.com.ar>
2023-06-03 13:54:53 -0300

    creación de rutas básicas y agregad ode Bootstrap

---
+++ src/pages/tiempo/[ciudad].tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+type StaticPropsParams ={
+  params:any;
+}
+
+const articulo = ({nombre}:any)=> {
+
+  return (
+    <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
+      <div><h1>{nombre}</h1></div>
+    </main>
+  )
+}
+
+export default articulo;
+
+export async function getStaticProps({params}:StaticPropsParams) {
+
+  const {ciudad} = params; 
+  const nombre = `Informe del tiempo segun este dato: "${ciudad}"`;
+
+  return{
+      props: {
+        nombre,
+      }
+    }
+}
+
+export async function getStaticPaths() {
+
+  return{
+      paths: [], //configuración para que no genere pre-rendering de las páginas
+      fallback: 'blocking',
+  };
+
+}
